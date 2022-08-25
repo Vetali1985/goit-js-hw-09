ef7961d0d6b56b31d156a3b8e1cd876f9ee72909
Vetali1985 <geelycross1985@gmail.com>
2022-08-25 19:49:38 +0300

    12

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,48 @@
 
+const refs = {
+    btnStart: document.querySelector( ` [data-start]`),
+    btnStop : document.querySelector('[data-stop]'),
+}
+const changeColor = {
+    timerID : null,
+    isDisable: false,
+    start() {
+        if (this.isDisable) {
+            return;
+        }
+        this.isDisable = true;
+    this.timerID =  setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor(),
+            console.log(`click`)
+    }, 1000);
+        
+    },
+
+    stop() {
+       this.isDisable = false;
+        clearInterval(this.timerID);
+        console.log(`stop`)
+     
+    
+        },
+
+}
+refs.btnStart.addEventListener(`click`, changeColor.start)
+refs.btnStop.addEventListener(`click`, changeColor.stop)
+
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    
+}
+
+
+
+
+
+
+
+
+
