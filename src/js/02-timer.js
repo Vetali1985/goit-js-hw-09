f08f57075b604e45975910b8572b9f2444ae3382
Vetali1985 <geelycross1985@gmail.com>
2022-09-03 15:56:08 +0300

    1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,12 +4,13 @@ import "flatpickr/dist/flatpickr.min.css";
 // import '../css/common.css';
 import Notiflix from 'notiflix';
 const refs = {
-    startBtn: document.querySelector(' [data-start]'),
-    days:document.querySelector(' [data-days]'),
-    hours:document.querySelector(' [data-hours]'),
-    minutes:document.querySelector(' [data-minutes]'),
+    startBtn: document.querySelector(' button[data-start]'),
+    days: document.querySelector(' [data-days]'),
+    hours: document.querySelector(' [data-hours]'),
+    minutes: document.querySelector(' [data-minutes]'),
     seconds: document.querySelector(' [data-seconds]'),
-    input:document.querySelector('#datetime-picker')
+    input: document.querySelector('#datetime-picker'),
+    rt: document.querySelector('button')
 }
 refs.startBtn.addEventListener(`click`, () => {
     timer.start()
@@ -21,17 +22,18 @@ refs.input.addEventListener(`input`, () => {
 
 
 
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        
+refs.rt.setAttribute("disabled", "disabled")
         console.log(selectedDates[0]);
     },
 };
-    
+
 new flatpickr(refs.input, options)
 
 
@@ -44,32 +46,32 @@ const timer = {
             return
         }
         const startTime = new Date(refs.input.value);
-        
+
         this.isActive = true;
 
         if (startTime < new Date()) {
             console.log(startTime === Date.now())
-            
+
             Notiflix.Notify.warning("Please choose a date in the future");
             return
         }
-        
-        
+
+
         this.intervalId = setInterval(() => {
             const currentTime = Date.now()
             const deltaTime = startTime - currentTime;
             if (deltaTime < 1000) {
-            clearInterval(this.intervalId)
+                clearInterval(this.intervalId)
             }
             const { days, hours, minutes, seconds } = convertMs(deltaTime);
             updateTime(days, hours, minutes, seconds);
         }, 1000);
-        
+
     },
-    
+
 };
 
- 
+
 
 
 
@@ -82,25 +84,25 @@ function updateTime(days, hours, minutes, seconds) {
 
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-  // Remaining days
-  const days =  addLeadingZero(Math.floor(ms / day));
-  // Remaining hours
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+    // Remaining days
+    const days = addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
-  return { days, hours, minutes, seconds };
+    return { days, hours, minutes, seconds };
 }
 
 function addLeadingZero(value) {
-    return String(value).padStart(2,`0`)
-    
+    return String(value).padStart(2, `0`)
+
 }
